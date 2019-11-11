af710e26641881df3f68506661d6260f325e3a34
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 14:14:44 +0300

    add list-manager controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,12 @@ import { TodoItem } from "./interfaces/todo-item";
   selector: "app-root",
   template: `
     <h1>Welcome to {{ title }}!</h1>
-    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
-    <ul>
-      <li *ngFor="let todoItem of todoList">
-        <app-todo-item [item]="todoItem"></app-todo-item>
-      </li>
-    </ul>
+    <app-list-manager></app-list-manager>
   `,
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "todo-list";
+  title = "My To Do List APP";
   todoList: TodoItem[] = [
     { title: "install NodeJS" },
     { title: "install Angular CLI" },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
+import { InputButtonUnitComponent } from "./input-button-unit/input-button-unit.component";
+import { TodoItemComponent } from "./todo-item/todo-item.component";
+import { ListManagerComponent } from "./list-manager/list-manager.component";
 
 @NgModule({
-  declarations: [AppComponent, InputButtonUnitComponent, TodoItemComponent],
+  declarations: [
+    AppComponent,
+    InputButtonUnitComponent,
+    TodoItemComponent,
+    ListManagerComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
