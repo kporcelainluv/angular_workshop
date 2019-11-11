40ca5c8439e68c14e014a40bac9353d47ee29203
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 11:36:57 +0300

    add class methods

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
-  styleUrls: ['./input-button-unit.component.css']
+  selector: "app-input-button-unit",
+  templateUrl: "./input-button-unit.component.html",
+  styleUrls: ["./input-button-unit.component.css"]
 })
 export class InputButtonUnitComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  title = "Hello World";
+  constructor() {
+    this.changeTitle("My First Angular App");
   }
 
+  ngOnInit() {}
+  changeTitle(newTitle: string) {
+    this.title = newTitle;
+  }
 }
