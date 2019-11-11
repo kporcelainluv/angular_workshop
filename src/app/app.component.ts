ccdc9c8bfefff6b170ef9910f2df4a34a191c701
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 14:31:02 +0300

    add css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { TodoItem } from "./interfaces/todo-item";
 @Component({
   selector: "app-root",
   template: `
-    <h1>Welcome to {{ title }}!</h1>
+    <h1 class="app-title">Welcome to {{ title }}!</h1>
+
     <app-list-manager></app-list-manager>
   `,
   styleUrls: ["./app.component.css"]
