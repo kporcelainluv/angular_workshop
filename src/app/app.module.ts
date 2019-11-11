6c8590816ab3826ae372de9f8c91fed6ed5cbb52
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 12:34:54 +0300

    add to-do component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent, InputButtonUnitComponent],
+  declarations: [AppComponent, InputButtonUnitComponent, TodoItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
