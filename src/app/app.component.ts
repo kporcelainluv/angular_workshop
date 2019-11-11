9f7a4703192b28b3bb3bfff08373e620382b5162
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 11:24:18 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <app-input-button-unit></app-input-button-unit>
+  `,
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = "todo-list";
 }
