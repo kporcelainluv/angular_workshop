15de48864a4f7c35e9fa34b58e781cc477c309ad
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-11 14:54:51 +0300

    add local storage

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,25 +1,40 @@
 import { Injectable } from "@angular/core";
 import { TodoItem } from "../interfaces/todo-item";
+import { StorageService } from "./storage.service";
 
+const todoListStorageKey = "Todo_List";
 @Injectable({
   providedIn: "root"
 })
 export class TodoListService {
-  private todoList: TodoItem[] = [
-    { title: "install NodeJS" },
-    { title: "install Angular CLI" },
-    { title: "create new app" },
-    { title: "serve app" },
-    { title: "develop app" },
-    { title: "deploy app" }
-  ];
+  todoList: TodoItem[];
 
-  constructor() {}
-
-  getTodoList() {
-    return this.todoList;
+  constructor(private storageService: StorageService) {
+    this.todoList =
+      storageService.getData(todoListStorageKey) || defaultTodoList;
   }
+
+  saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
+
   addItem(item: TodoItem) {
     this.todoList.push(item);
+    this.saveList();
+  }
+
+  updateItem(item, changes) {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = { ...item, ...changes };
+    this.saveList();
+  }
+
+  deleteItem(item) {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
+  }
+  getTodoList() {
+    return this.todoList;
   }
 }
